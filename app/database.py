6a74842d94a0591d99a574672192e10c86c18b6d
Andres <andres.03.12.2003@gmail.com>
2025-05-31 20:47:35 -0400

    Refactor configuración de DATABASE_URL y carga de variables de entorno

--- app/database.py
+++ app/database.py
@@ -2,16 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+from dotenv import load_dotenv
+load_dotenv()
+print("✅ BASE DE DATOS LEIDA:", repr(os.getenv("DATABASE_URL")))
 
-# 🚨 Configura tu URL de PostgreSQL aquí
-# DATABASE_URL = os.getenv("DATABASE_URL", DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/backend_app")
-# DATABASE_URL = os.getenv(
-#     "DATABASE_URL",
-#     "postgresql://postgres:postgres@localhost:5432/backend_app"
-# )
-DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:jrc033@localhost:5432/ProyectoAndres")
-# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://bd_gnerate_frontends_user:xDLVHjaqX1JaFHq8PrRgjKMI45lGN2Iw@dpg-d08quvs9c44c73dpmt1g-a.oregon-postgres.render.com/bd_gnerate_frontends")
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/backend_app"
+)
 
 # Crear engine
 engine = create_engine(DATABASE_URL, echo=True)
